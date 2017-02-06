cba4e01bdb7de2e30fe62c4176d6c6bdaa091bc0
Wenjie Wang <wenjie.2.wang@uconn.edu>
2017-02-05 23:19:41 -0500

    tweak ui

--- miniSplines2/app.R
+++ miniSplines2/app.R
@@ -9,46 +9,56 @@ ui <- miniPage(
     miniTabstripPanel(
         miniTabPanel("Input", icon = icon("sliders"),
                      miniContentPanel(
-                         selectInput("fun", "Splines",
+                         selectInput("fun", "Spline Function",
                                      c("B-splines", "M-splines",
                                        "I-splines", "C-splines")),
-                         selectInput("nKnots", "Number of Internal Knots",
-                                     seq.int(0, 5), 3),
-                         selectInput("degree", "Degree", seq.int(0, 4), 2))),
+                         selectInput("nKnots",
+                                     paste("Number of Internal Knots",
+                                           "(equidistant)"),
+                                     seq.int(0, 8), 3),
+                         selectInput("degree", "Degree", seq.int(0, 3), 2),
+                         checkboxInput('intercept', 'Intercept', value = TRUE),
+                         sliderInput("lwd", "Line Width", min = 0.5, max = 3,
+                                     value = 1.5, step = 0.5))),
         miniTabPanel("Output", icon = icon("table"),
                      miniContentPanel(DT::dataTableOutput("table"))),
         miniTabPanel("Visualize", icon = icon("area-chart"),
-                     miniContentPanel(plotOutput("matplot", height = "100%")))
+                     miniContentPanel(padding = 0,
+                                      plotOutput("matplot", height = "100%"))),
+        selected = "Visualize"
     )
 )
 
 
 server <- function(input, output, session)
 {
-    x <- seq.int(0, 0.99, 0.01)
+    x <- seq.int(0, 1, 0.01)
     output$matplot <- renderPlot({
-        df <- as.numeric(input$nKnots) + as.numeric(input$degree) + 1L
         fun <- switch(input$fun,
                       "B-splines" = "bSpline",
                       "M-splines" = "mSpline",
                       "I-splines" = "iSpline",
                       "C-splines" = "cSpline")
+        df <- as.integer(input$nKnots) + as.integer(input$degree) +
+            as.integer(input$intercept)
         resMat <- do.call(fun, list(x = x, df = df, degree = input$degree,
-                                    intercept = TRUE, Boundary.knots = c(0, 1)))
-        matplot(x, resMat, type = "l",
-                xlab = "x", ylab = "Spline Bases")
-        abline(v = attr(resMat, "knots"), col = "gray", lty = 2)
+                                    intercept = input$intercept))
+        matplot(x, resMat, type = "l", xlab = "x",
+                ylab = "Spline Bases", lwd = input$lwd)
+        abline(v = attr(resMat, "knots"), col = "gray",
+               lty = 2, lwd = input$lwd)
     })
 
     output$table <- DT::renderDataTable({
-        df <- as.numeric(input$nKnots) + as.numeric(input$degree) + 1L
         fun <- switch(input$fun,
                       "B-splines" = "bSpline",
                       "M-splines" = "mSpline",
                       "I-splines" = "iSpline",
                       "C-splines" = "cSpline")
+        df <- as.integer(input$nKnots) + as.integer(input$degree) +
+            as.integer(input$intercept)
         resMat <- do.call(fun, list(x = x, df = df, degree = input$degree,
-                                    intercept = TRUE, Boundary.knots = c(0, 1)))
+                                    intercept = input$intercept))
         outDat <- data.frame(round(resMat, 3))
         colnames(outDat) <- paste("Spline", seq_len(df))
         cbind(x = x, outDat)
